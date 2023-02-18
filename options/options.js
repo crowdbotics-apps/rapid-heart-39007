59a3aa207b8ec000430332bde5b57306fa23894d
Crowdbotics <team@crowdbotics.com>
2023-02-18 17:23:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-heart-39007",
+  url: "https://rapid-heart-39007.botics.co",
+  api: "https://rapid-heart-39007.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
